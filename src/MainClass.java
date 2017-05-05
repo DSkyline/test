e860758373cfce07dd129d8bd7b4edefdae88ec2
DSkyline <skyline984@gmail.com>
2017-05-05 22:42:18 +0200

    Update MainClass.java

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,8 +2,8 @@
 public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		
 		System.err.println("New line added");
+		System.err.println("Third line added");
 	}
 
 }
