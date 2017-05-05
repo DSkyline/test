68c34ad234a7be26889e7bd5aa8e108e19fe992c
DSkyline <skyline984@gmail.com>
2017-05-05 23:04:49 +0200

    Update MainClass.java

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,7 +2,6 @@
 public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("Intentionaly creating conflict");
 		System.err.println("New line added");
 		System.err.println("Third line added");
 	}
